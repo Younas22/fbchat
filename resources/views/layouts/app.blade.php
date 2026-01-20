3cc59319a797838b392c82a1d05e6000804001b4
Younas22 <hm.younas22@gmail.com>
2026-01-20 20:25:52 +0500

    i

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Facebook Chat Manager')</title>
+    <title>@yield('title', 'Facebook Chat Manager1211')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50">
